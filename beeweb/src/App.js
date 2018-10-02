6ab36cbc65f2f1683f3912a1729e3f012dce7b63
rose <rot>
2018-10-02 10:57:48 +0700

    rose developing save working point

--- beeweb/src/App.js
+++ beeweb/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">TEST REACT</h1>
-        </header>
-        <p className="App-intro">
-         happy 
-        </p>
+      // <div className="App">
+
+      // <div className="App-header">
+      // <div className="Logo-header">
+      //   <img src={logo} className="App-logo" alt="logo" />
+      // </div>
+           
+      //      <span className="Title-header"></span>
+      // </div>
+       
+      //   <p className="App-intro">
+      // !!!   happy New year 2019 !!!
+      //   </p>
+      // </div>
+
+      <div className="App-header">
+      <table className="Title-Table">
+        <tr>
+          <td className="Title-Tdleft"  ><img src={logo} className="App-logo" alt="logo" /></td>
+          <td ></td>
+        </tr>
+      </table>
+
       </div>
     );
   }
